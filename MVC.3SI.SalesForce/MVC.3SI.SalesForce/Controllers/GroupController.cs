e5dc83da01a696d3a12998eb0cbd6b8eb01327dc
anhddse02700 <anhddse02700@gmail.com>
2016-07-20 11:40:36 +0700

    Update Group Page

---
+++ MVC.3SI.SalesForce/MVC.3SI.SalesForce/Controllers/GroupController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC._3SI.SalesForce.Models;
+using MVC._3SI.SalesForce.Infrastructure;
+
+namespace MVC._3SI.SalesForce.Controllers
+{
+    public class GroupController : Controller
+    {
+        //
+        // GET: /Group/
+        public ActionResult Index()
+        {
+            var groupInfo = new GroupModel(Convert.ToString(Session[Constants.AccessTokenSession]));
+            return View(groupInfo);
+        }
+	}
+}
---
+++ MVC.3SI.SalesForce/MVC.3SI.SalesForce/Models/GroupModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC._3SI.SalesForce.Infrastructure.Salesforce;
+using Newtonsoft.Json;
+using MVC._3SI.SalesForce.Entity.Salesforce;
+namespace MVC._3SI.SalesForce.Models
+{
+
+
+    //=======================================================================
+    public class GroupModel
+    {
+        public Groups.GroupList Grouplist;
+        public GroupModel() {
+        }
+
+        public GroupModel(string accessToken)
+        {
+            var apiServices = new ApiService(accessToken);
+            var groupJson = apiServices.GetUserGroup();
+            // Convert json string to C# object 
+            Grouplist = JsonConvert.DeserializeObject<Groups.GroupList>(groupJson);
+        }
+    }
+}
